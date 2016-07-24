40ef63f0a1107cee1c5a90f9297e047d424e045b
ikosenn <i@kosenn.me>
2016-07-24 22:23:26 +0300

    Color maker with switches

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeColorComponent(sender: AnyObject) {
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
